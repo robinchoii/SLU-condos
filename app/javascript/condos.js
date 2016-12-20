0c2f565246f21514a76a99e69cf10299b662e968
robinchoii <robinchoii@gmail.com>
2016-12-19 21:15:11 -0800

    add javascript folder, move  array of condo objects to separate file

---
+++ app/javascript/condos.js
@@ -0,0 +1,130 @@
+  var condos = [
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    },
+    {
+      sqft: '750',
+      bedroom: '2',
+      bathroom: '1',
+      price: '55000'
+    }
+  ];
+
+  module.exports = condos
